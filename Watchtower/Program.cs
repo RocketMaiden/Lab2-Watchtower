6076813ffe95081ac5f0900b85ff548379096623
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-03-20 20:42:37 +0200

    [wip] added tuples for enemy position

--- Watchtower/Program.cs
+++ Watchtower/Program.cs
@@ -1,22 +1,24 @@
-﻿Console.WriteLine("Please enter X coordinate of an enemy");
-int x = int.Parse(Console.ReadLine());
+﻿(int, int) enemyPosition;
+bool enemyAlert = false;
+
+Console.WriteLine("Please enter X coordinate of an enemy");
+enemyPosition.Item1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Please enter Y coordinate of an enemy");
-int y = int.Parse(Console.ReadLine());
+enemyPosition.Item2 = int.Parse(Console.ReadLine());
 
 string response;
-bool enemyPosition;
 
-response = enemyPosition switch
+response = enemyAlert switch
 {
-    (x < 0 && y < 0) => "The enemy is to the SW",
-    (x == 0 && y < 0) => "The enemy is to the S",
-    (x > 0 && y < 0) => "The enemy is to the SE",
-    (x < 0 && y == 0) => "The enemy is to the W",
-    (x == 0 && y == 0) => "The enemy is here",
-    (x > 0 && y == 0) => "The enemy is to the E",
-    (x > 0 && y > 0) => "The enemy is to the NW",
-    (x == 0 && y > 0) => "The enemy is to the N",
-    (x > 0 && y > 0) => "The enemy is to the NE",
+    (enemyPosition.Item1 < 0 && enemyPosition.Item2 < 0) => "The enemy is to the SW",
+    (enemyPosition.Item1 == 0 && enemyPosition.Item2 < 0) => "The enemy is to the S",
+    (enemyPosition.Item1 > 0 && enemyPosition.Item2 < 0) => "The enemy is to the SE",
+    (enemyPosition.Item1 < 0 && enemyPosition.Item2 == 0) => "The enemy is to the W",
+    (enemyPosition.Item1 == 0 && enemyPosition.Item2 == 0) => "The enemy is here",
+    (enemyPosition.Item1 > 0 && enemyPosition.Item2 == 0) => "The enemy is to the E",
+    (enemyPosition.Item1 > 0 && enemyPosition.Item2 > 0) => "The enemy is to the NW",
+    (enemyPosition.Item1 == 0 && enemyPosition.Item2 > 0) => "The enemy is to the N",
+    (enemyPosition.Item1 > 0 && enemyPosition.Item2 > 0) => "The enemy is to the NE",
     _ => "Invalid data"
 };
 
