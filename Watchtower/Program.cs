327f8b8672b9fbe64a5458528430b1777d120914
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-03-20 20:56:50 +0200

    changed arms of switch expression

--- Watchtower/Program.cs
+++ Watchtower/Program.cs
@@ -1,5 +1,4 @@
 ﻿(int, int) enemyPosition;
-bool enemyAlert = false;
 
 Console.WriteLine("Please enter X coordinate of an enemy");
 enemyPosition.Item1 = int.Parse(Console.ReadLine());
@@ -8,18 +7,17 @@ enemyPosition.Item2 = int.Parse(Console.ReadLine());
 
 string response;
 
-response = enemyAlert switch
+response = enemyPosition switch
 {
-    (enemyPosition.Item1 < 0 && enemyPosition.Item2 < 0) => "The enemy is to the SW",
-    (enemyPosition.Item1 == 0 && enemyPosition.Item2 < 0) => "The enemy is to the S",
-    (enemyPosition.Item1 > 0 && enemyPosition.Item2 < 0) => "The enemy is to the SE",
-    (enemyPosition.Item1 < 0 && enemyPosition.Item2 == 0) => "The enemy is to the W",
-    (enemyPosition.Item1 == 0 && enemyPosition.Item2 == 0) => "The enemy is here",
-    (enemyPosition.Item1 > 0 && enemyPosition.Item2 == 0) => "The enemy is to the E",
-    (enemyPosition.Item1 > 0 && enemyPosition.Item2 > 0) => "The enemy is to the NW",
-    (enemyPosition.Item1 == 0 && enemyPosition.Item2 > 0) => "The enemy is to the N",
-    (enemyPosition.Item1 > 0 && enemyPosition.Item2 > 0) => "The enemy is to the NE",
-    _ => "Invalid data"
+    (< 0 , < 0) => "The enemy is to the SW",
+    (0 , < 0) => "The enemy is to the S",
+    ( > 0 , < 0) => "The enemy is to the SE",
+    ( < 0 , 0) => "The enemy is to the W",
+    ( 0 , 0) => "The enemy is here",
+    ( > 0, 0) => "The enemy is to the E",
+    ( < 0, > 0) => "The enemy is to the NW",
+    (0 , > 0) => "The enemy is to the N",
+    ( > 0 , > 0) => "The enemy is to the NE"
 };
 
 Console.WriteLine(response);
