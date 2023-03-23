196707b7d92ea71a392defa992eea2905c8fd7fa
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-03-23 21:34:46 +0200

    converted code to Main style

--- Watchtower/Program.cs
+++ Watchtower/Program.cs
@@ -1,24 +1,30 @@
-﻿(int, int) enemyPosition;
-
-Console.WriteLine("Please enter X coordinate of an enemy");
-enemyPosition.Item1 = int.Parse(Console.ReadLine());
-Console.WriteLine("Please enter Y coordinate of an enemy");
-enemyPosition.Item2 = int.Parse(Console.ReadLine());
-
-string response;
-
-response = enemyPosition switch
+﻿internal class Program
 {
-    (< 0 , < 0) => "The enemy is to the SW",
-    (0 , < 0) => "The enemy is to the S",
-    ( > 0 , < 0) => "The enemy is to the SE",
-    ( < 0 , 0) => "The enemy is to the W",
-    ( 0 , 0) => "The enemy is here",
-    ( > 0, 0) => "The enemy is to the E",
-    ( < 0, > 0) => "The enemy is to the NW",
-    (0 , > 0) => "The enemy is to the N",
-    ( > 0 , > 0) => "The enemy is to the NE"
-};
+    private static void Main(string[] args)
+    {
+        (int, int) enemyPosition;
 
-Console.WriteLine(response);
-Console.ReadLine();
+        Console.WriteLine("Please enter X coordinate of an enemy");
+        enemyPosition.Item1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter Y coordinate of an enemy");
+        enemyPosition.Item2 = int.Parse(Console.ReadLine());
+
+        string response;
+
+        response = enemyPosition switch
+        {
+            ( < 0, < 0) => "The enemy is to the SW",
+            (0, < 0) => "The enemy is to the S",
+            ( > 0, < 0) => "The enemy is to the SE",
+            ( < 0, 0) => "The enemy is to the W",
+            (0, 0) => "The enemy is here",
+            ( > 0, 0) => "The enemy is to the E",
+            ( < 0, > 0) => "The enemy is to the NW",
+            (0, > 0) => "The enemy is to the N",
+            ( > 0, > 0) => "The enemy is to the NE"
+        };
+
+        Console.WriteLine(response);
+        Console.ReadLine();
+    }
+}
