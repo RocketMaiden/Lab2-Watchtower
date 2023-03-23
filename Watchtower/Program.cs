86fb7f3207cc796fde88167dbad3ea87e2163375
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-03-23 22:39:19 +0200

    added random enemy position, user input verification

--- Watchtower/Program.cs
+++ Watchtower/Program.cs
@@ -2,16 +2,49 @@
 {
     private static void Main(string[] args)
     {
+        (int, int) towerPosition;
         (int, int) enemyPosition;
-
-        Console.WriteLine("Please enter X coordinate of an enemy");
-        enemyPosition.Item1 = int.Parse(Console.ReadLine());
-        Console.WriteLine("Please enter Y coordinate of an enemy");
-        enemyPosition.Item2 = int.Parse(Console.ReadLine());
-
+        (int, int) enemyRelativePosition;
         string response;
 
-        response = enemyPosition switch
+        Console.WriteLine("Enter watchtower position. It should stay in range from -5 to 5");
+
+        Console.WriteLine("Enter watchtower X");
+        
+        while (true)
+        {
+            towerPosition.Item1 = ReadUserInput("not a digit - please, enter a digit");
+            if (towerPosition.Item1 <= 5 && towerPosition.Item1 >= - 5)
+            {
+                Console.WriteLine($"Your tower coordinate X is {towerPosition.Item1}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Coordinate is not in the range from -5 to 5");
+            }
+        }
+        
+        Console.WriteLine("Enter watchtower Y");
+
+        while (true)
+        {
+            towerPosition.Item2 = ReadUserInput("not a digit - please, enter a digit");
+            if (towerPosition.Item2 <= 5 && towerPosition.Item2 >= - 5)
+            {
+                Console.WriteLine($"Your tower coordinate Y is {towerPosition.Item2}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Coordinate is not in the range from -5 to 5");
+            }
+        }
+
+        enemyPosition = SetEnemyPosition();
+        enemyRelativePosition = GetEnemyRelativePosition(enemyPosition, towerPosition);
+
+        response = enemyRelativePosition switch
         {
             ( < 0, < 0) => "The enemy is to the SW",
             (0, < 0) => "The enemy is to the S",
@@ -26,5 +59,36 @@
 
         Console.WriteLine(response);
         Console.ReadLine();
+    }
+
+    static int ReadUserInput(string exceptionMessage)
+    {
+        while (true)
+        {
+            if (int.TryParse(Console.ReadLine(), out int result))
+            {
+                return result;
+            }
+            else
+            {
+                Console.WriteLine(exceptionMessage);
+            }
+        }
+    }
+
+    static (int, int) SetEnemyPosition()
+    {
+        int randomX = new Random().Next(-5, 6);
+        int randomY = new Random().Next(-5, 6);
+        
+        return(randomX, randomY);
+    }
+
+    static (int, int) GetEnemyRelativePosition((int, int) enemyPosition, (int, int) towerPosition)
+    {
+        int relativeEnemyX = towerPosition.Item1 - enemyPosition.Item1;
+        int relativeEnemyY = towerPosition.Item2 - enemyPosition.Item2;
+        
+        return (relativeEnemyX, relativeEnemyY);
     }
 }
